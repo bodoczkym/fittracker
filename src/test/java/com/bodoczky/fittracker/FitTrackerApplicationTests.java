d2782bac40d846304a544814ef1ed0f3ee62072d
bodoczkym <shaddox27@yahoo.com>
2026-02-07 14:46:17 +0100

    feat(backend): Initial backend project setup with Spring Boot 4.0.2

---
+++ src/test/java/com/bodoczky/fittracker/FitTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bodoczky.fittracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
